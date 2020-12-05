c21f7d113c5cba6fb8fa35abf262c75266a55b7f
Jan <janplucinsky@gmail.com>
2020-12-05 13:38:23 +0100

    init

---
+++ resources/js/scritp.js
@@ -0,0 +1,25 @@
+$('document').ready(function() {
+
+   
+/* For the sticky nav */
+    $('.js--section-features').waypoint(function(direction){
+
+        if(direction =="down"){
+
+            $('nav').addClass('sticky');
+
+       } else {
+
+            $('nav').removeClass('sticky');
+
+        }
+
+    });
+   
+
+/* Scroll on buttons */
+
+$('.js--scroll-to-plans').click(function() {
+    $('html,body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
+});
+});
